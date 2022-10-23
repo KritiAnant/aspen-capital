4558f471d2a685e82a7e8d99c63222716d5f15c1
KritiAnant <kanant@andrew.cmu.edu>
2022-10-23 13:54:59 -0400

    initial commit

--- micro-etl-app/app/app.py
+++ micro-etl-app/app/app.py
@@ -2,19 +2,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this
-# software and associated documentation files (the "Software"), to deal in the Software
-# without restriction, including without limitation the rights to use, copy, modify,
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
+
 
 import boto3
 import logging
@@ -22,56 +10,142 @@ import pandas as pd
 import os
 import requests
 import io
+import boto3
+import datetime
 
 # Environment variables
-URL = os.environ['Url']
 S3_BUCKET = os.environ['S3Bucket']
 LOG_LEVEL = os.environ['LogLevel']
-FILENAME = os.environ['Filename']
 
 # Log settings
 logger = logging.getLogger()
 logger.setLevel(LOG_LEVEL)
 
-# Function to support partial requests
-def range_header(start=0, end=0):    
-    if start < 0 and end == 0:
-        byte_range = 'bytes=%s' % (start)
-    else:        
-        byte_range = 'bytes=%s-%s' % (start, end)
-
-    return {'Range': byte_range}
 
 # Lambda function handler
 def lambda_handler(event, context):
     logger.info('## EVENT')
     logger.info(event)
-    
-    columns = ['Date', 'Region_Name', 'Area_Code', 'Detached_Average_Price',
-       'Detached_Index', 'Detached_Monthly_Change', 'Detached_Annual_Change',
-       'Semi_Detached_Average_Price', 'Semi_Detached_Index',
-       'Semi_Detached_Monthly_Change', 'Semi_Detached_Annual_Change',
-       'Terraced_Average_Price', 'Terraced_Index', 'Terraced_Monthly_Change',
-       'Terraced_Annual_Change', 'Flat_Average_Price', 'Flat_Index',
-       'Flat_Monthly_Change', 'Flat_Annual_Change']
-    
-    # Request to get the last 2000000 bytes to get the most recent data in the CSV skipping the first row
-    # Implies that the value are in ascending order with most recent at the end of the file
-    res = requests.get(URL, headers=range_header(-2000000), allow_redirects=True)
-    df = pd.read_csv(io.StringIO(res.content.decode('utf-8')), engine='python', error_bad_lines=False, names=columns, skiprows=1)
-    logger.info('## NUMBER OF ELEMENTS')
-    logger.info(df.size)
-    
-    # Extract only values in a specified time range
-    start_date = '2018-01-01'
-    end_date = '2018-12-31'
-    date_range = (df['Date'] >= start_date) & (df['Date'] <= end_date)
-    df = df[date_range]
-    logger.info('## NUMBER OF ELEMENTS IN THE RANGE')
-    logger.info(df.size)
 
-    # Save files into S3
-    url = 's3://{}/{}'.format(S3_BUCKET, FILENAME)
-    df.to_csv(url)
-    logger.info('## FILE PATH')
-    logger.info(url)
+
+    # ## Read file
+
+    # In[2]:
+
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id='AKIARQWNMHB5GCUWG22K',
+        aws_secret_access_key='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'
+    )
+
+    # In[3]:
+
+    obj = s3.get_object(
+        Bucket='aspen-capital-raw-data',
+        Key='data_engineer_raw_data.xlsx')
+
+    # In[4]:
+
+    data = obj['Body'].read()
+    # print(data)
+    borrower = pd.read_excel(data, engine='openpyxl', sheet_name='borrower')
+    # display(borrower.head())
+
+    # In[5]:
+
+    role_profile = pd.read_excel(data, engine='openpyxl', sheet_name='role_profile')
+    # display(role_profile.head())
+
+    # # ETL
+
+    # ### USER_PROFILE
+
+    # In[6]:
+
+    user_profile_df = borrower.copy()
+    user_profile_df = user_profile_df.rename(columns={"id": "user_profile_id"})
+    user_profile_df = user_profile_df[user_profile_df['user_profile_id'].notna()]
+    user_profile_df = user_profile_df[user_profile_df['full_name'].notna()]
+    user_profile_df[['first_name', 'last_name']] = user_profile_df.full_name.str.split(n=1, expand=True)
+    user_profile_df['created_date'] = datetime.datetime.now()
+    user_profile_df['created_by'] = 'Kriti'
+    user_profile_df.drop(['full_name', 'street', 'city', 'state', 'zip_code', 'phone_cell', 'email'], axis=1,
+                         inplace=True)
+    user_profile_df['updated_date'] = datetime.datetime.now()
+    user_profile_df['updated_by'] = 'Kriti'
+    # user_profile_df.head()
+
+    # In[7]:
+
+    csv_buffer = io.StringIO()
+    user_profile_df.to_csv(csv_buffer)
+
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object('aspen-capital-user-profile', 'user_profile.csv').put(Body=csv_buffer.getvalue());
+
+    # ### USER
+
+    # In[8]:
+
+    user_df = user_profile_df.copy()
+    user_df['user_id'] = user_df.index
+    user_df.drop(['first_name', 'last_name'], axis=1, inplace=True)
+    user_df = user_df.rename(columns={"created_date": "created", "updated_date": "updated"})
+    # user_df.head()
+
+    # In[9]:
+
+    csv_buffer = io.StringIO()
+    user_df.to_csv(csv_buffer)
+
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object('aspen-capital-user', 'user.csv').put(Body=csv_buffer.getvalue());
+
+    # ### ROLE_PROFILE_TYPE
+    #
+    #
+
+    # In[10]:
+
+    role_profile_type_df = role_profile.copy()
+    role_profile_type_df = role_profile_type_df.rename(
+        columns={"borrower_id": "role_profile_type_id", "role_profile": "type"})
+    role_profile_type_df['created'] = datetime.datetime.now()
+    role_profile_type_df['created_by'] = 'Kriti'
+    role_profile_type_df['updated'] = datetime.datetime.now()
+    role_profile_type_df['updated_by'] = 'Kriti'
+    # role_profile_type_df.head()
+
+    # In[11]:
+
+    csv_buffer = io.StringIO()
+    role_profile_type_df.to_csv(csv_buffer)
+
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object('aspen-capital-role-profile-type', 'role-profile_type.csv').put(Body=csv_buffer.getvalue());
+
+    # ### ROLE_PROFILE
+
+    # In[12]:
+
+    role_profile_df = pd.DataFrame(user_df.loc[:, ['user_profile_id', 'user_id']].set_index('user_profile_id')
+                                   ).join(role_profile_type_df.set_index('role_profile_type_id'), how='outer')
+    # pd.concat([user_df.loc[:,['user_profile_id','user_id']].set_index('user_profile_id'), role_profile_type_df.set_index('role_profile_type_id')], axis=1).reset_index()
+    role_profile_df['role_profile_type_id'] = role_profile_df.index
+    role_profile_df = role_profile_df.reset_index()
+    role_profile_df['role_profile_id'] = role_profile_df.index
+    role_profile_df = role_profile_df.drop(['type', 'created', 'created_by', 'updated', 'updated_by', 'index'], axis=1)
+    role_profile_df['created'] = datetime.datetime.now()
+    role_profile_df['created_by'] = 'Kriti'
+    role_profile_df['updated'] = datetime.datetime.now()
+    role_profile_df['updated_by'] = 'Kriti'
+    # role_profile_df.head()
+
+    # In[13]:
+
+    csv_buffer = io.StringIO()
+    role_profile_df.to_csv(csv_buffer)
+
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object('aspen-capital-role-profile', 'role_profile.csv').put(Body=csv_buffer.getvalue());
+    
